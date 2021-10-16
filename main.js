4c6d75c1f9207f84a21be2a4885930d882283c58
Catherine-prog-byte <77283675+Catherine-prog-byte@users.noreply.github.com>
2021-10-16 18:51:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
-function preload() {
+
+function setup() {
+    video=createCapture(VIDEO);
+    video.size(500,500);
+    canvas=createCanvas(500,500);
+    canvas.position(560,200);
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
 }
 function draw() {
+    background("#46435C");
 }
-function setUp() {
-    canvas = createCanvas(300 , 300 );
-    canvas.position(500 , 160);
+function modelLoaded() {
+    console.log('model is loaded');
+}
+function gotPoses(results) {
+    if (results.length>0) {
+        console.log(results);
+    }
 }
