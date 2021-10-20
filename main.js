cddf9fde93d36da3c1cfdd6b97e68271e1f57f92
Catherine-prog-byte <77283675+Catherine-prog-byte@users.noreply.github.com>
2021-10-20 18:46:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-
+noseX = 0;
+noseY = 0;
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
 function setup() {
     video=createCapture(VIDEO);
     video.size(500,500);
@@ -9,6 +13,10 @@ function setup() {
 }
 function draw() {
     background("#46435C");
+    document.getElementById("square_side").innerHTML="width and height of the square is = "+difference+"px";
+    fill("#6AEAB0");
+    stroke("#6AEAB0");
+    square(noseX,noseY,difference);
 }
 function modelLoaded() {
     console.log('model is loaded');
@@ -16,5 +24,12 @@ function modelLoaded() {
 function gotPoses(results) {
     if (results.length>0) {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        console.log("noseX="+noseX+", noseY="+noseY);
+        console.log("leftWristX="+leftWristX+", rightWristX="+rightWristX);
+        difference=floor(leftWristX-rightWristX);
     }
 }
